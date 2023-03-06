69a937f3b071d026d56194ba0ec3caa8aecfe37e
cglavin50 <80028065+cglavin50@users.noreply.github.com>
2023-03-05 17:28:08 -0800

    Updated tester.go to handle miller-rabin primality test with explicit a parameter. Added function pmod (power then mod) to avoid code duplication across miller-rabin and p-1 tests

--- tester.go
+++ tester.go
@@ -29,18 +29,16 @@ func gcd(a int, n int) int {
 	return small
 }
 
-func ptest(a int, n int) int {
-	counter := 1
+func ptest(a int, n int) int { // test primality of value n using int a
+	counter := 1 // test if counter starts at 0 or 1 using new pmod func
 	val := gcd(a, n)
 
 	for val == 1 {
-		oldA := a
-		for i := 0; i < counter; i++ {
-			a *= oldA
-			a %= n
-			if a < 0 {
-				a += n
-			}
+		pmod(&a, counter, n)
+		//fmt.Println("a:", a)
+		if a == 1 {
+			val = -1
+			break
 		}
 		val = gcd(a-1, n)
 		counter++
@@ -53,9 +51,57 @@ func ptest(a int, n int) int {
 	}
 }
 
+func pmod(a *int, i int, n int) { //a^i mod n
+	// fmt.Println("pmod")
+	// fmt.Println("a:", *a)
+	oldA := *a
+	for I := 1; I < i; I++ {
+		*a *= oldA
+		*a %= n
+		if *a < 0 {
+			*a += n
+		}
+	}
+	// fmt.Println("a:", *a)
+
+} // returns a^i mod n
+
+func millerRabin(a int, n int) bool { // return true if probably prime, false for composite, same as p-1, testing primality of n using a
+	N := n
+	n--
+	k := n / 2
+	n /= 2
+	counter := 1
+	for n%2 == 0 {
+		k = n / 2
+		n /= 2
+		counter++
+	} // have k, use N for original n
+	pmod(&a, k, N)
+
+	if a == N-1 || a&N == 1 {
+		return true
+	}
+	pmod(&a, 2, N)
+	for i := 1; i < counter; i++ { //loop from b1 to b_i-1
+		if a%N == 1 {
+			return false
+		}
+		if a%N == N-1 {
+			return true
+		}
+		pmod(&a, 2, N)
+	}
+	if a%n == N-1 {
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 	if len(os.Args) < 4 {
-		fmt.Println("Usage: test(ex gcd, p-1) a n")
+		fmt.Println("Usage: test(ex gcd, p-1) n a")
 		return
 	}
 	a, _ := strconv.Atoi(os.Args[3])
@@ -64,13 +110,19 @@ func main() {
 	switch os.Args[1] {
 	case "gcd": // breaks unneeded in go
 		v := gcd(a, n)
-		fmt.Println("gcd(", a, ",", n, ") = ", v)
+		fmt.Println("gcd(", a, ",", n, ") =", v)
 	case "p-1":
 		v := ptest(a, n)
 		if v == -1 {
 			fmt.Println("Test inconclusive")
 		} else {
-			fmt.Println(n, " is composite, divisible by ", v)
+			fmt.Println(n, "is composite, divisible by", v)
+		}
+	case "miller-rabin":
+		if millerRabin(a, n) {
+			fmt.Println(n, "is probably prime")
+		} else {
+			fmt.Println(n, "is composite")
 		}
 	default:
 		fmt.Println("Unexpected test passed in, error")
